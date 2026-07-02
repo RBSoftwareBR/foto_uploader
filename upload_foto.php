477c8a49969503d85465c64f18e8cca849b13e9c
RBSoftwareBR <contatorbsoftware@gmail.com>
2026-07-02 17:48:46 -0300

    Updating method

--- upload_foto.php
+++ upload_foto.php
@@ -2,7 +2,9 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
+use Google\Auth\HttpHandler\HttpHandlerFactory;
 use Google\Cloud\Storage\StorageClient;
+use GuzzleHttp\Client;
 
 header('Content-Type: application/json');
 
@@ -23,11 +25,30 @@ try {
         throw new Exception('Formato de imagem inválido.');
     }
 
-    $bucketName = 'SEU_PROJECT_ID.firebasestorage.app';
-    // Em buckets antigos pode ser: SEU_PROJECT_ID.appspot.com
+    $keyFilePath = __DIR__ . '/config/firebase-service-account.json';
+    $cacertPath = __DIR__ . '/config/cacert.pem';
+
+    $credenciais = json_decode(file_get_contents($keyFilePath), true);
+    if (!$credenciais || empty($credenciais['project_id'])) {
+        throw new Exception('Arquivo de credenciais Firebase inválido.');
+    }
+
+    $projectId = $credenciais['project_id'];
+    // gs://transportes-thomaz.appspot.com
+    $bucketName = 'transportes-thomaz.appspot.com';
+
+    $guzzleOptions = [];
+    if (file_exists($cacertPath)) {
+        $guzzleOptions['verify'] = $cacertPath;
+    }
+
+    $httpHandler = HttpHandlerFactory::build(new Client($guzzleOptions));
 
     $storage = new StorageClient([
-        'keyFilePath' => __DIR__ . '/config/firebase-service-account.json'
+        'keyFilePath' => $keyFilePath,
+        'projectId' => $projectId,
+        'authHttpHandler' => $httpHandler,
+        'httpHandler' => $httpHandler,
     ]);
 
     $bucket = $storage->bucket($bucketName);
@@ -39,19 +60,23 @@ try {
     };
 
     $nomeArquivo = 'formularios/fotos/' . date('Y/m/d/') . uniqid('foto_', true) . '.' . $extensao;
+    $downloadToken = bin2hex(random_bytes(16));
 
     $bucket->upload(
         fopen($foto['tmp_name'], 'r'),
         [
             'name' => $nomeArquivo,
             'metadata' => [
-                'contentType' => $foto['type']
-            ]
+                'contentType' => $foto['type'],
+                'metadata' => [
+                    'firebaseStorageDownloadTokens' => $downloadToken,
+                ],
+            ],
         ]
     );
 
-    // URL pública, caso o bucket permita leitura pública
-    $url = "https://storage.googleapis.com/{$bucketName}/ERP_EKT_THOMAZ/{$nomeArquivo}";
+    $pathEncoded = rawurlencode($nomeArquivo);
+    $url = "https://firebasestorage.googleapis.com/v0/b/{$bucketName}/o/{$pathEncoded}?alt=media&token={$downloadToken}";
 
     echo json_encode([
         'success' => true,
